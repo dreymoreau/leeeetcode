06d32568dd967bbc2560f6d099bfc96ac83fed7a
dreymotoh <striclyspoken@gmail.com>
2023-08-12 00:01:33 -0400

    coding challenge

---
+++ contains-duplicate/contains-duplicate.js
@@ -0,0 +1,19 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+// sort in ascending order
+// loop through
+// if an element nums[i] == nums[i+1] return true
+// if not return false
+var containsDuplicate = function (nums) {
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == nums[i + 1]) {
+      return true;
+    }
+  }
+  return false;
+};
