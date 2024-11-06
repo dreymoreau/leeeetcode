17664e5cbd4bf621d6a2716096d3a1374ee69505
dreymotoh <striclyspoken@gmail.com>
2024-11-06 05:35:38 -0500

    coding challenge

--- contains-duplicate/contains-duplicate.js
+++ contains-duplicate/contains-duplicate.js
@@ -17,3 +17,26 @@ var containsDuplicate = function (nums) {
   }
   return false;
 };
+
+// ou cool!!!!! check this updated way :0 HASHMAP!!!
+
+//  will always be an array,, will always be number values in vthe array,, never any letters or other objects,, arrays etc.
+// returning true if any value appears at least twice in the array and return false if ever element is distinct
+// Input: nums = [1,2,3,1]
+// Output: true
+// Input: nums = [1,2,3,4]
+// Output: false
+
+// create an empty object called obj
+// use the for of loop to check if the element already exists in the object return true,, if not then assign the value of true to the element in the obj object
+var containsDuplicate = function(nums) {
+  let obj = {}
+  for(let key of nums){
+      if(obj[key]){
+          return true
+      } else {
+          obj[key] = true
+      }
+  }
+  return false
+};
